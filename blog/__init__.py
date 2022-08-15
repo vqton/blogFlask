13012681930b5287973612007f1b1701c86eb6e3
vqton <vuquangton@outlook.com>
2022-08-15 11:24:38 +0700

    Implement flask login package

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from . import routes, models
