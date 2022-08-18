3183f2dc86a8000df843297290600d9fa72f294d
vqton <vuquangton@outlook.com>
2022-08-18 15:16:54 +0700

    audit init code

--- blog/__init__.py
+++ blog/__init__.py
@@ -18,20 +18,21 @@ login = LoginManager(app)
 login.login_view = "login"
 bootstrap = Bootstrap(app)
 mail = Mail(app)
-from . import routes, models, errors
+
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
         auth = None
         if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
             auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-        if app.config["MAIL_USER_TLS"]:
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
             fromaddr="no-reply@" + app.config["MAIL_SERVER"],
             toaddrs=app.config["ADMINS"],
-            subject="Blog Failure",
+            subject="Microblog Failure",
             credentials=auth,
             secure=secure,
         )
@@ -39,7 +40,7 @@ if not app.debug:
         app.logger.addHandler(mail_handler)
 
     if not os.path.exists("logs"):
-        os.mkdirs("logs")
+        os.mkdir("logs")
     file_handler = RotatingFileHandler(
         "logs/microblog.log", maxBytes=10240, backupCount=10
     )
@@ -53,3 +54,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info("Microblog startup")
+
+from blog import routes, models, errors
